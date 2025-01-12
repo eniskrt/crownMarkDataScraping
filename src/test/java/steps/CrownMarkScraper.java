2e60a97937ede26ed828054dc7de7a859194d361
enskerti <enskerti@gmail.com>
2025-01-12 17:15:55 +0300

    gitignore updated, fileoutput function created, all categories created

--- src/test/java/steps/CrownMarkScraper.java
+++ src/test/java/steps/CrownMarkScraper.java
@@ -1,24 +1,33 @@
 package steps;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class CrownMarkScraper extends TestBase {
 
+    private static List<String[]> allProductsInformation = new ArrayList<>(); // Ortak veri listesi
     @Test
     public void bedroom() throws InterruptedException {
         Actions actions = new Actions(driver);
 
         WebElement bedroomCategoryTab = driver.findElement(By.xpath("(//div[@class='TitleAreaLink'])[1]"));
-        actions.moveToElement(bedroomCategoryTab).perform();
-        Thread.sleep(1000);
-
-        WebElement allBedroomsLink = driver.findElement(By.xpath("(//div[@id='Bedrooms-Submenu']//div[@class='SubmenuLink'])[4]/a"));
-        allBedroomsLink.click();
+        bedroomCategoryTab.click();
 
         //Pagination Information
         String lastPageCountAsString = driver.findElement(By.xpath("//*[@id='CatalogContentArea']/table/tbody/tr[2]/td/b/a[6]/u")).getText();
@@ -56,25 +65,171 @@ public class CrownMarkScraper extends TestBase {
         Actions actions = new Actions(driver);
 
         WebElement diningCategoryTab = driver.findElement(By.xpath("(//div[@class='TitleAreaLink'])[2]"));
-        actions.moveToElement(diningCategoryTab).perform();
-        Thread.sleep(1000);
+        diningCategoryTab.click();
 
-        WebElement allDiningLink = driver.findElement(By.xpath("(//div[@id='DiningRooms-Submenu']//div[@class='SubmenuLink'])[4]/a"));
-        allDiningLink.click();
+        String lastPageCountAsString = driver.findElement(By.xpath("//*[@id='CatalogContentArea']/table/tbody/tr[2]/td/b/a[6]/u")).getText();
+        int pageSize = Integer.parseInt(lastPageCountAsString);
+
+        for (int k = 1; k <= pageSize; k++){
+
+            List<WebElement> productList = driver.findElements(By.xpath("//div[@id='CatalogContentArea']/table/tbody/tr/td/a/img"));
+
+            WebElement product;
+            for (int i = 1; i <= productList.size(); i++){
+                product = productList.get(i - 1);
+                product.click();
+                Thread.sleep(1000);
+                driver.navigate().back();
+            }
+
+            if (k == pageSize){
+                break;
+            }
+
+            WebElement nextPageBtn = driver.findElement(By.xpath("(//td[@align='right'])[2]//a//u[text()='Next']"));
+            try {
+                nextPageBtn.click();
+            } catch (Exception e) {
+                actions.click(nextPageBtn).perform();
+            }
+            Thread.sleep(1000); // Sayfanın yüklenmesi için bekleme süresi
+
+        }
     }
 
     @Test
-    public void livingRoom() {
+    public void livingRoom() throws InterruptedException {
         Actions actions = new Actions(driver);
+
+        WebElement livingRoomCategoryTab = driver.findElement(By.xpath("(//div[@class='TitleAreaLink'])[3]"));
+        livingRoomCategoryTab.click();
+
+        String lastPageCountAsString = driver.findElement(By.xpath("//*[@id='CatalogContentArea']/table[2]/tbody/tr[2]/td/b/a[5]/u")).getText();
+        int pageSize = Integer.parseInt(lastPageCountAsString);
+
+        for (int k = 1; k <= pageSize; k++){
+
+            List<WebElement> productList = driver.findElements(By.xpath("//div[@id='CatalogContentArea']/table/tbody/tr/td/a/img"));
+
+            WebElement product;
+            for (int i = 1; i <= productList.size(); i++){
+                product = productList.get(i - 1);
+                product.click();
+                Thread.sleep(1000);
+                driver.navigate().back();
+            }
+
+            if (k == pageSize){
+                break;
+            }
+
+            WebElement nextPageBtn = driver.findElement(By.xpath("(//td[@align='right'])[2]//a//u[text()='Next']"));
+            try {
+                nextPageBtn.click();
+            } catch (Exception e) {
+                actions.click(nextPageBtn).perform();
+            }
+            Thread.sleep(1000); // Sayfanın yüklenmesi için bekleme süresi
+
+        }
     }
 
     @Test
-    public void homeOffice() {
+    public void homeOffice() throws InterruptedException {
         Actions actions = new Actions(driver);
+
+        WebElement homeOfficeCategoryTab = driver.findElement(By.xpath("(//div[@class='TitleAreaLink'])[4]"));
+        homeOfficeCategoryTab.click();
+
+        //There is no product in this category now. Control is only for no product.
+        String expectedContent = "No Items have been found.";
+        WebElement noContentElement = driver.findElement(By.xpath("//*[@id='CatalogContentArea']/table[2]/tbody/tr[3]/td/b"));
+        String noContentText = noContentElement.getText();
+        Assert.assertEquals(expectedContent,noContentText);
     }
 
     @Test
-    public void accessories() {
+    public void accessories() throws InterruptedException {
         Actions actions = new Actions(driver);
+
+        WebElement accessoriesCategoryTab = driver.findElement(By.xpath("(//div[@class='TitleAreaLink'])[5]"));
+        accessoriesCategoryTab.click();
+
+        String lastPageCountAsString = driver.findElement(By.xpath("//*[@id='CatalogContentArea']/table[2]/tbody/tr[2]/td/b/a[4]/u")).getText();
+        int pageSize = Integer.parseInt(lastPageCountAsString);
+
+        for (int k = 1; k <= pageSize; k++){
+
+            List<WebElement> productList = driver.findElements(By.xpath("//div[@id='CatalogContentArea']/table/tbody/tr/td/a/img"));
+
+            WebElement product;
+            for (int i = 1; i <= productList.size(); i++){
+                product = productList.get(i - 1);
+                product.click();
+                Thread.sleep(1000);
+                driver.navigate().back();
+            }
+
+            if (k == pageSize){
+                break;
+            }
+
+            WebElement nextPageBtn = driver.findElement(By.xpath("(//td[@align='right'])[2]//a//u[text()='Next']"));
+            try {
+                nextPageBtn.click();
+            } catch (Exception e) {
+                actions.click(nextPageBtn).perform();
+            }
+            Thread.sleep(1000); // Sayfanın yüklenmesi için bekleme süresi
+
+        }
+    }
+
+    @AfterClass
+    public static void writeDataToExcel() throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Product List");
+
+        // Başlık stili
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex()); // Arka plan rengi
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND); // Dolgu tipi
+        Font headerFont = workbook.createFont();
+        headerFont.setBold(true); // Kalın yazı
+        headerStyle.setFont(headerFont);
+
+        // Başlıklar
+        String[] headers = {"SKU NUMBER", "DESCRIPTION", "STOCK LEVEL", "BASE PRICE", "SPECIAL PRICE", "STATUS"};
+        Row headerRow = sheet.createRow(0);
+        for (int i = 0; i < headers.length; i++) {
+            Cell headerCell = headerRow.createCell(i);
+            headerCell.setCellValue(headers[i]);
+            headerCell.setCellStyle(headerStyle); // Stili uygula
+        }
+
+        // Verileri ekle
+        int rowCount = 1;
+        for (String[] rowData : allProductsInformation) {
+            Row row = sheet.createRow(rowCount++);
+            for (int i = 0; i < rowData.length; i++) {
+                row.createCell(i).setCellValue(rowData[i]);
+            }
+        }
+
+        // Sütun genişliklerini otomatik ayarla
+        for (int i = 0; i < headers.length; i++) {
+            sheet.autoSizeColumn(i);
+        }
+
+        // Excel dosyasını kaydet
+        String currentDate = new SimpleDateFormat("ddMMyyyy").format(new Date());
+        LocalTime localTime = LocalTime.now();
+        //String currentHour = localTime.format(DateTimeFormatter.ofPattern("HH:mm"));
+        String fileName = "CrownMarkPriceList_" + currentDate + ".xlsx";
+        try (FileOutputStream outputStream = new FileOutputStream(fileName)) {
+            workbook.write(outputStream);
+        }
+        workbook.close();
+        System.out.println("Excel dosyası başarıyla kaydedildi!");
     }
 }
